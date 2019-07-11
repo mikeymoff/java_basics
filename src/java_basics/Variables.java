a04fc57317494713c0d012cbbc07a548d33cc871
mikeymoff <47802812+mikeymoff@users.noreply.github.com>
2019-07-10 23:04:33 -0600

    Variables and data sets

---
+++ src/java_basics/Variables.java
@@ -0,0 +1,33 @@
+package java_basics;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		
+		int x = 4;
+		
+		System.out.println(x);
+		
+		x = 23;
+		
+		System.out.println(x + 4);
+		
+	   String words = "This is a string";
+		System.out.println(words);
+
+
+		long bigNumber = 10000000000L; //For large numbers needs L at end
+		
+		short smallNumber = 32767; //Max number for short shown
+		
+		byte reallysmallNumber = 127; //Max number for byte shown
+		
+		double decimalNumber = 394.949;
+		
+		boolean decision = true;
+		
+		char letter = '%'; //Single key only
+		
+	}
+
+}
